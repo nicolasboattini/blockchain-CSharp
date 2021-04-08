145da8e750f614636cd0217818bb59c6d99ad742
nicoboattini <nicoboattini@gmail.com>
2021-04-08 00:11:52 -0300

    Implementación de funcion de hasheo en sha256 para bloques

--- Blockchain/Bloque.cs
+++ Blockchain/Bloque.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Blockchain
 {
@@ -8,10 +9,23 @@ namespace Blockchain
         public String Nombre { get; set; }
         public String Motivo { get; set; }
         public String FileHash { get; set; }
-        public String Hash { get; set; }
+        public String Hash;
         public String PrevHash { get; set; }
         public DateTime Fecha { get; set; }
-        public Bloque() { 
+
+        private void SetHash(string phash)
+        {
+            this.Hash = phash;
+        }
+        public string GetHash()
+        {
+            return this.Hash;
+        }
+        public void ModHash(string phash)
+        {
+            this.SetHash(phash);
+        }
+        public Bloque() {
         }
         public Bloque(long pindi, string pnom, string pmot, string pfhash, string phash, string pprehash)
         {
@@ -19,9 +33,21 @@ namespace Blockchain
             this.Indice = pindi;
             this.Nombre = pnom;
             this.Motivo = pmot;
-            this.FileHash = pfhash;|
-            this.Hash = phash;
+            this.FileHash = pfhash;
             this.PrevHash = pprehash;
+            this.Hash = phash;
+        }
+
+        public override string ToString()
+        {   
+            return string.Concat(Indice, Nombre, Motivo, FileHash, PrevHash, Fecha.ToString());
+        }
+
+        public byte[] ToByteArray()
+        {
+            var cadena = this.ToString();
+            byte[] bytes = Encoding.UTF8.GetBytes(cadena);
+            return bytes;
         }
     }
 }
--- Blockchain/Manager.cs
+++ Blockchain/Manager.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace Blockchain
 {
     
     class Manager
     {
+        
         public List<Bloque> BlockChain { get; set; }
         public int i = 0;
         public string prehash;
@@ -29,21 +31,40 @@ namespace Blockchain
         {
             BlockChain = new List<Bloque>();
         }
-        public void CrearBlockchain(string pnom, string pmot, string pfhash, string phash)
+        public void CrearBlockchain(string pnom, string pmot, string pfhash)
         {
             if (geti() == 0)
             {
-                Bloque genesis = new Bloque(geti(), pnom, pmot, pfhash, phash, "000000");
+                
+                Bloque genesis = new Bloque(geti(), pnom, pmot, pfhash, "0", "000000");
+                byte[] genbyte = genesis.ToByteArray();
+                string genhash = SHA256maker(genbyte);
+                genesis.ModHash(genhash);
                 BlockChain.Add(genesis);
                 incrementar_i();
             } else
             {
-                prehash = BlockChain.ElementAt(geti()-1).Hash;
-                Bloque block = new Bloque(geti(), pnom, pmot, pfhash, phash, prehash);
+                string prehash = BlockChain.ElementAt(geti()-1).GetHash();
+                Bloque block = new Bloque(geti(), pnom, pmot, pfhash, "0", prehash);
+                byte[] blockbyte = block.ToByteArray();
+                string blockhash = SHA256maker(blockbyte);
+                block.ModHash(blockhash);
                 BlockChain.Add(block);
                 incrementar_i();
             }
             
+        }
+
+        static string SHA256maker(byte[] cryptobyte)
+        {
+            var crypt = new SHA256Managed();
+            var hash = new StringBuilder();
+            byte[] crypto = crypt.ComputeHash(cryptobyte);
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            return hash.ToString();
         }
     }
 }
